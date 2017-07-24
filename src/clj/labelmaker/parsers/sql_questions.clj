2e1c7f1c76dde80cb2a375a503462f4125070f12
Paul Gowder <paul.gowder@gmail.com>
2017-07-24 15:26:11 -0400

    database schema

--- src/clj/labelmaker/parsers/sql_questions.clj
+++ src/clj/labelmaker/parsers/sql_questions.clj
@@ -2,23 +2,12 @@
 
 ;; this namespace needs to:
 ;; 1.  add each question to the questions table (incrementing qid each time)
-;; 2.  store qid for each question in a map somewhere in state so that the data entry front-end can be constructed
+;; 2.  then just query the table to get the data I just stored, and stick it all in a map somewhere to send to the front end and build functions to send back in.  
 
 ;; I want a table just of questions
 ;; and a table of mc-answer options mapped to questions
 ;; and a table of responses
 
-;; I'm going to have to manually increment the primary keys in order to make this work with 
+;; I'm going to have to manually increment the question primary keys in order to hold them in state I think.
 
-(def question-mapping {:qid "INTEGER PRIMARY KEY"
-                       :question "TEXT"})
-
-(def mc-answer-opts-mapping {:aid "INTEGER PRIMARY KEY"
-                             :qid "INTEGER"
-                             :answeropt "TEXT"})
-
-
-(defn response-mapping [respkey]
-  {:id "SERIAL PRIMARY KEY"
-   :qid "INTEGER"
-   :answer (apicker respkey)})
+;; also need a separate document parser that grabs every document, adds it to documents table, and marks it either as coded or not coded
