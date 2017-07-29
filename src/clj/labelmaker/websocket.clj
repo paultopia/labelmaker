d9f748883fb330f902bc57d38460c061febf54cc
Paul Gowder <paul.gowder@gmail.com>
2017-07-29 12:50:19 -0400

    a little more core async gone in on the receiving end to handle incoming ws

--- src/clj/labelmaker/websocket.clj
+++ src/clj/labelmaker/websocket.clj
@@ -1,10 +1,13 @@
 (ns labelmaker.websocket
   (:require [immutant.web.async :as async]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.core.async
+             :refer [>! <! >!! <!! go chan thread go-loop buffer]]))
 
 (defn to-json [data] (json/write-str data))
 (defn from-json [data] (json/read-str data :key-fn keyword))
 
+(def inchan (chan 100))
 
 (defonce sockets (atom #{}))
 
@@ -14,16 +17,29 @@
 (defn disconnect! [socket {:keys [code reason]}]
   (swap! sockets #(remove #{socket} %)))
 
-(defn notify-clients! [this-socket msg]
+(defn take-incoming [this-socket msg]
   (let [message (from-json msg)]
-    (doseq [socket @sockets]
-      (async/send! socket (to-json {:word (:word message) :num (inc (:num message))})))))
+    (go
+      (>! inchan {:socket this-socket :message message}))))
+
+(defn dispatch-outgoing [this-socket message]
+  (doseq [socket @sockets]
+    (async/send! socket (to-json {:word (:word message) :num (inc (:num message))}))))
+
+(defn listen! [callback]
+  (thread
+    (loop []
+      (let [{:keys [socket message]} (<!! inchan)]
+        (callback socket message))
+      (recur))))
+
+(listen! dispatch-outgoing)
 
 (def websocket-callbacks
   "WebSocket callback functions"
   {:on-open connect!
    :on-close disconnect!
-   :on-message notify-clients!})
+   :on-message take-incoming})
 
 (defn ws-handler [request]  
   (async/as-channel request websocket-callbacks))
