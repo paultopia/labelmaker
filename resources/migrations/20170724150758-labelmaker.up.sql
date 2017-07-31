76e934d70a6bae5ad38b62aa585ebaa7205773ad
Paul Gowder <paul.gowder@gmail.com>
2017-07-31 15:37:50 -0400

    questions are yaml now not some silly custom format

--- resources/migrations/20170724150758-labelmaker.up.sql
+++ resources/migrations/20170724150758-labelmaker.up.sql
@@ -3,11 +3,12 @@ CREATE TYPE anstype as ENUM ('tf', 'multiplechoice', 'freeanswer', 'numerical');
 CREATE TABLE questions
 (qid SERIAL PRIMARY KEY,
  question TEXT,
- instructions TEXT,
- preemptive BOOLEAN,
- highlight BOOLEAN,
+ instructions TEXT DEFAULT NULL,
+ preemptive BOOLEAN DEFAULT FALSE,
+ highlight BOOLEAN DEFAULT FALSE,
  answertype anstype,
- ansoptions TEXT,
+ ansoptions TEXT DEFAULT NULL,
+ validation TEXT DEFAULT NULL,
  dateadded TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE documents
@@ -22,14 +23,15 @@ CREATE TABLE responses
 (id SERIAL PRIMARY KEY,
  qid INTEGER,
  did INTEGER,
- userid VARCHAR(20),
+ uid INTEGER,
  answer TEXT,
  dateadded TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  hasreentry BOOLEAN DEFAULT FALSE,
  isreentry BOOLEAN DEFAULT FALSE);
 
 CREATE TABLE users
-(username VARCHAR(20) PRIMARY KEY,
- password VARCHAR(20),
+(uid SERIAL PRIMARY KEY
+username TEXT,
+ password TEXT,
  isadmin BOOLEAN,
  dateadded TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
--- src/clj/labelmaker/parsers/questions.clj
+++ src/clj/labelmaker/parsers/questions.clj
@@ -1,29 +1,5 @@
 (ns labelmaker.parsers.questions
-  (:require [clojure.string :refer [split trim]]
-            [clj-yaml.core :as yaml]))
-
-
-(defn parse-alist [alist]
-  (let [answers (mapv trim (split alist #"-"))]
-    (println (count answers))
-    (vec (rest answers))))
-
-(defn parse-block [hblock]
-  (let [[header text] (split (trim hblock) #"\n" 2)
-        item (if (= header "answeropts")
-               (parse-alist text)
-               text)]
-    {(keyword header) item}))
-
-(defn parse-1q [question]
-  (let [hblocks (rest (split (trim question) #"#"))]  ; to get rid of leading blank line from earlier split.
-    (apply merge (map parse-block hblocks))))
+  (:require [clj-yaml.core :as yaml]))
 
 (defn parse-questions! []
-  (let [qfile (trim (slurp "questions.txt"))
-        questions (split qfile #"\n\n")
-        qparts (mapv parse-1q questions)]
-    qparts))
-
-(defn yaml-parse []
   (-> "questions.yml" slurp yaml/parse-string))
